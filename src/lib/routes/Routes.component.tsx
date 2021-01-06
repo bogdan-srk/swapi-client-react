3d04888294fa780f852db09f2930f3b4c675e36e
Bogdan <srk.bogdan@gmail.com>
2021-01-06 22:02:15 +0200

    initial

---
+++ src/lib/routes/Routes.component.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+
+type Props = {
+
+};
+type State = {
+
+};
+
+export class Routes extends React.Component<Props, State> {
+  render() {
+    return (
+      <div>
+        <span>Routes</span>
+      </div>
+    );
+  };
+};
